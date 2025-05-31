7fc5b2e38342c34e134d92e209f49c392fe9a7de
Thanh-Long V. Le <thanhlongtdk2000@gmail.com>
2025-05-31 11:00:12 +0900

    Modify script to run with conda

--- open-r1-intuitor/run_grpo.sh
+++ open-r1-intuitor/run_grpo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source ./openr1/bin/activate
-export WANDB_API_KEY=YOUR_WANDB_API_KEY
+conda activate llm
+export WANDB_API_KEY="4afeb050111c67af2b2b80f0741d5233f38a8910"
 export ACCELERATE_LOG_LEVEL=info
 
 # Run vllm-serve in the background with nohup
@@ -10,8 +10,8 @@ VLLM_PID=$!
 echo "vLLM server started with PID: $VLLM_PID"
 
 # Run accelerate launch in the background with nohup
-nohup env CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 ACCELERATE_LOG_LEVEL=info \
-    accelerate launch --config_file recipes/accelerate_configs/zero2.yaml --num_processes=7 \
+nohup env NCCL_DEBUG=INFO CUDA_VISIBLE_DEVICES=1,2,3 ACCELERATE_LOG_LEVEL=info \
+    accelerate launch --config_file recipes/accelerate_configs/zero2.yaml --num_processes=3 \
     src/open_r1/grpo.py --config recipes/Qwen2.5-3B/grpo/config_demo.yaml --wandb_project open-r1 --run_name Qwen2.5-GRPO-3B > run_grpo.log 2>&1 &
 TRAINING_PID=$!
 echo "Training process started with PID: $TRAINING_PID"
--- open-r1-intuitor/run_intuitor.sh
+++ open-r1-intuitor/run_intuitor.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-source ./openr1/bin/activate
-export WANDB_API_KEY=YOUR_WANDB_API_KEY
+conda activate llm
+export WANDB_API_KEY="4afeb050111c67af2b2b80f0741d5233f38a8910"
 export ACCELERATE_LOG_LEVEL=info
 
 # Run vllm-serve in the background with nohup
-nohup env CUDA_VISIBLE_DEVICES=0 trl vllm-serve --model "Qwen/Qwen2.5-3B" > vllm-serve.log 2>&1 &
-VLLM_PID=$!
-echo "vLLM server started with PID: $VLLM_PID"
+# nohup env CUDA_VISIBLE_DEVICES=0 trl vllm-serve --model "Qwen/Qwen2.5-3B" > vllm-serve.log 2>&1 &
+# VLLM_PID=$!
+# echo "vLLM server started with PID: $VLLM_PID"
 
 # Run accelerate launch in the background with nohup
-nohup env CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 ACCELERATE_LOG_LEVEL=info\
-    accelerate launch --config_file recipes/accelerate_configs/zero2.yaml --num_processes=7 \
+nohup env CUDA_VISIBLE_DEVICES=1,2,3 ACCELERATE_LOG_LEVEL=info\
+    accelerate launch --config_file recipes/accelerate_configs/zero2.yaml --num_processes=3 \
     src/open_r1/intuitor.py --config recipes/Qwen2.5-3B/intuitor/config_demo.yaml --wandb_project open-r1 --run_name Qwen2.5-Intuitor-3B > run_intuitor.log 2>&1 &
 TRAINING_PID=$!
 echo "Training process started with PID: $TRAINING_PID"
